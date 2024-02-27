ee837a1faced6852e5c6c55d0d5eb0c5893c1dff
Luke Mai <n11373661@qut.edu.au>
2024-02-27 20:59:57 +1000

    Adding app and retailer page

---
+++ app/retailer/page.tsx
@@ -0,0 +1,7 @@
+export default function ReatilerPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>Retailer Page</div>
+        </main>
+    );
+}
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -39,9 +39,6 @@ export default function Navbar() {
         setUserinfo({});
     }
 
-
-    
-
     const loggedInNavBar = () => {
         return (
             <div>
